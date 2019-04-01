5deb22f131ab64359ce64ace10f97d1d31c86d2c
Luke Welton <luke.welton97@gmail.com>
2019-04-01 15:45:07 -0500

    add dev button to make logging in easier

--- views/LoginView.js
+++ views/LoginView.js
@@ -32,6 +32,14 @@ export default class LoginView extends React.Component {
         });
     }
 
+    _devAuthenticate() {
+        Firebase.auth().signInWithEmailAndPassword("musicmattersbookings@gmail.com", "Mus1cMatter$").then(() => {
+            this.props.navigation.navigate("Loading");
+        }).catch(err => {
+            alert(err.toString());
+        })
+    }
+
     render() {
         return (
             <AppContainer>
@@ -65,6 +73,16 @@ export default class LoginView extends React.Component {
                             title = "Log In"
                             color = "blue"
                             onPress = {() => this._authenticate()}
+                        />
+
+                        {/* This makes it easier to log in as Mr. Moody.
+
+                                THIS MUST BE DELETED BEFORE PUBLISHING!
+                        */}
+                        <Button
+                            title = "Dev Mode"
+                            color = "red"
+                            onPress = {() => this._devAuthenticate()}
                         />
                     </View>
                 </View>
